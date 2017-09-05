cee0f27525a19751210b6c0db73235d730dae78a
yeahpython <tommyli@stanford.edu>
2017-09-05 12:40:06 +0800

    saving before cleanup

--- extension/myscript.js
+++ extension/myscript.js
@@ -1,36 +1,43 @@
-var semanticModules = "cite, time, div, blockquote, sub, em, sup, p, li, td, strong, i, b, span, h1, h2, h3, h4, h5, h6, a, button";
+var semanticModules = "cite, time, div, blockquote, sub, em, sup, p, li, td, strong, i, b, span, h1, h2, h3, h4, h5, h6, a, button, footer, label, bdi";
+// semanticModules = "img"
 
 
+var windowWidth = $(window).width();
 
 
-$(semanticModules).each(function(){
-  /*var immediatelyContainedText = $(this).contents().not($(this).children()).filter(function() {
+function background_color_change(index, elem){
+  var $this = $(this)
+  return $this.css("background-color") != "rgba(0, 0, 0, 0)" &&
+  $this.css("background-color") != "none" &&
+  $this.css("display") != "none" &&
+  $this.css("visibility") != "hidden" &&
+  $this.css("background-color") != $(this).parent().css("background-color");
+}
+
+function contains_text(index,elem) {
+  var immediatelyContainedText = $(this).contents().not($(this).children()).filter(function() {
       return this.nodeType === 3; //Node.TEXT_NODE
     }).text();
-  if (immediatelyContainedText != "" || $(this).css("background-image") != "none") {
-    //$(this).css('border', 'solid 1px black');
-    $(this).css('background-color', 'rgba(255,0,0,0.05)');
-  } else {
-    //console.log(immediatelyContainedText)
-  }*/
-  // detect change in background color
-  if ( $(this).css("background-color") != "rgba(0, 0, 0, 0)" &&
-       $(this).css("background-color") != "none" &&
-       $(this).css("display") != "none" &&
-       $(this).css("visibility") != "hidden" &&
-       $(this).css("background-color") != $(this).parent().css("background-color")) {
-    $(this).css('border', 'dashed 3px black');
-  $(this).addClass('barrier');
-    //console.log($(this).css("background-color") + "|" + $(this).parent().css("background-color"));
-  }
-});
+  // if ($(this).attr("class") == "_NId") {
+  //   console.log("[" + immediatelyContainedText + "]" + (immediatelyContainedText != ""));
+  // }
+  // replaced whitespace
+  return immediatelyContainedText.replace(/\s+/g, '') != "";
+}
+
+$(semanticModules).filter(contains_text).addClass('barrier');
+
+
+
 
 // $("span").css('border', 'dashed 3px black');
 
-$("<div id='chrome-pet-box'>Hello</div>").appendTo(document.body);
+$("<div id='chrome-pet-box'></div>").appendTo(document.body);
 $("<div class='chrome-pet-positioner'></div>").appendTo($("#chrome-pet-box"));
 $("<div class='chrome-pet'>^_^</div>").appendTo($(".chrome-pet-positioner"));
 $("<div id='collision_illustrator'></div>").appendTo(document.body);
+$("<div id='collision_illustrator_2'></div>").appendTo(document.body);
+
 $("<div id='position_illustrator'></div>").appendTo(document.body);
 $("<div id='bad_position_illustrator'></div>").appendTo(document.body);
 
@@ -47,6 +54,10 @@ $("<img id='emoji'></img>").attr("src", chrome.extension.getURL('emojione/1f600.
 //         element["on" + eventName] = callback;
 //     }
 // }
+
+function randomizeEmoji(){
+  $("#emoji").attr("src", chrome.extension.getURL('emojione/1f6' + ("0" + Math.floor((Math.random() * 40))).slice(-2) + '.png'));
+}
 
 (function(){
 
@@ -71,29 +82,67 @@ $("<img id='emoji'></img>").attr("src", chrome.extension.getURL('emojione/1f600.
   //     }
   // });
 
-  window.addEventListener("keydown", function(e) {
+  // key_tracker = {37:false, 38:false, 39:false, 40:false}
+  var old_keydowns = {37:false, 38:false, 39:false, 40:false}
+  var keydowns     = {37:false, 38:false, 39:false, 40:false}
+  var jump_allowed = true
+ /* window.addEventListener("keydown", function(e) {
     // space and arrow keys
-    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+    if([37, 38, 39, 40].indexOf(e.keyCode) > -1) {
         e.preventDefault();
-        if (e.keyCode == 39) {
-          console.log("yo");
-          vx += 2;
-          // $(".chrome-pet").text(">_>")
-        } else if (e.keyCode == 37) {
-          vx -= 2;
-          // $(".chrome-pet").text("<_<")
-        } else if (e.keyCode == 38) {
-          vy = -10;
-          $("#emoji").attr("src", chrome.extension.getURL('emojione/1f6' + ("0" + Math.floor((Math.random() * 45))).slice(-2) + '.png')).appendTo($(".chrome-pet"));
-          // $(".chrome-pet").text("^_^")
-        }
+        keydowns[e.keyCode] = true;
     }
   }, false);
 
+  window.addEventListener("keyup", function(e) {
+    // space and arrow keys
+    if([37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+        keydowns[e.keyCode] = false;
+    }
+  }, false);*/
+
+
 
   var collided = [];
-
+  var last_best_index = -1;
   function timestep() {
+    for (var i = 37; i <40; i++) {
+      var rand = Math.random();
+      var threshold = 0.97;
+      if (i == 38) {
+        threshold = keydowns[38] ? 0.92 : 0.98
+      }
+      if (rand > threshold) {
+        keydowns[i] = !keydowns[i];
+        if (rand > 0.995) {
+          randomizeEmoji();
+        }
+      }
+    }
+
+
+    if (keydowns[39] && !keydowns[37]) {
+      vx = 4;
+      $("#emoji").css("transform", "rotate(30deg)");
+    } else if (keydowns[37] && !keydowns[39]) {
+      vx = -4;
+      $("#emoji").css("transform", "rotate(-30deg)");
+    } else {
+
+      $("#emoji").css("transform", "none");
+      if (vx > 0) {
+        vx = Math.max(vx - 1, 0);
+      } else if (vx < 0) {
+        vx = Math.min(vx + 1, 0);
+      }
+    }
+    if (keydowns[38] /*&& !old_keydowns[38]*/ && jump_allowed) {
+      vy = -10;
+      jump_allowed = false;
+    }
+    old_keydowns = {37:keydowns[37], 38:keydowns[38], 39:keydowns[39], 40:keydowns[40]}
+    // console.log(old_keydowns);
     // console.log("step");
     //physics step: move the box
     var y = parseInt($(".chrome-pet-positioner").css("top"), 10);
@@ -101,28 +150,49 @@ $("<img id='emoji'></img>").attr("src", chrome.extension.getURL('emojione/1f600.
 
     // var original_x = x;
     // var original_y = y;
-    smooth_accumulator += 0.4;
-    if (smooth_accumulator > 1) {
+    smooth_accumulator += 0.8;
+    // uhh
+    while (smooth_accumulator > 1) {
       vy += 1;
-      smooth_accumulator = 0.0;
+      smooth_accumulator -= 1.0;
     }
-    if (vy > 10) {
-      vy = 10;
+    if (vy > 9) {
+      vy = 9;
     }
-    if (vy < -10) {
-      vy = -10;
+    if (vy < -9) {
+      vy = -9;
     }
-    if (vx > 10) {
-      vx = 10;
+    if (vx > 9) {
+      vx = 9;
     }
-    if (vx < -10) {
-      vx = -10;
+    if (vx < -9) {
+      vx = -9;
     }
     // x += vx;
     // y += vy;
     // console.log("x:" + x + " y: " + y);
 
     // console.log(x + vx);
+    var window_top = $(window).scrollTop();
+    var window_left = $(window).scrollLeft();
+    var window_bottom = window_top + $(window).height();
+    var window_right = window_left + $(window).width();
+    if (y + vy < window_top) {
+      var new_y = window_top;
+      vy = new_y - y;
+    } else if (y + vy + $(".chrome-pet-positioner").outerHeight() > window_bottom) {
+      var new_y = window_bottom - $(".chrome-pet-positioner").outerHeight();
+      vy = new_y - y;
+      jump_allowed = true
+    } else if (x + vx < window_left) {
+      var new_x = window_left;
+      vx = new_x - x;
+      jump_allowed = true
+    } else if (x + vx + $(".chrome-pet-positioner").outerWidth() > window_right) {
+      var new_x = window_right - $(".chrome-pet-positioner").outerWidth();
+      vx = new_x - x;
+      jump_allowed = true
+    }
 
     $(".chrome-pet-positioner").css("top", y + vy);
     $(".chrome-pet-positioner").css("left", x + vx);
@@ -131,9 +201,11 @@ $("<img id='emoji'></img>").attr("src", chrome.extension.getURL('emojione/1f600.
     // var oy = parseInt($('.chrome-pet-positioner').offset().top, 10);
     // console.log("ox:" + ox + "oy" + oy);
 
-    $barriers = $(".barrier").not(":hidden").slice(0,50);
-    console.log($barriers.size());
+    $barriers = $(".barrier").not(":hidden").slice(0,200);
+    // console.log($barriers.size());
     // projecting step
+
+
     $barriers.each(function(){
       var position = $(this).offset();
       var size = {height:parseInt($(this).outerHeight(),10), width:parseInt($(this).outerWidth(), 10)};
@@ -145,13 +217,24 @@ $("<img id='emoji'></img>").attr("src", chrome.extension.getURL('emojione/1f600.
       position.top = parseInt(position.top, 10);
 
       // a is true when we are doing vertical bars
-      // b is true when we are top left
+      // b is true when we are bottom right
       for (var a = 0; a < 2; a++) {
         for (var b = 0; b < 2; b++) {
-          var new_position = {left: (a && !b) ? position.left + size.width : position.left,
-                              top:  (a || !b) ? position.top : position.top + size.height};
-          var new_size = {width: a ? 0 : size.width,
-                          height: a ? size.height : 0};
+          if (0 /* using lines */) {
+            var new_position = {left: (a && !b) ? position.left + size.width : position.left,
+                                top:  (a || !b) ? position.top : position.top + size.height};
+            var new_size = {width: a ? 0 : size.width,
+                            height: a ? size.height : 0};
+          } else {
+            if (a || b) {
+              continue;
+            } else {
+              var new_position = {left: position.left,
+                                  top:  position.top};
+              var new_size = {width: size.width,
+                              height: size.height};
+            }
+          }
 
 
           // console.log("block: " + new_position);
@@ -194,22 +277,48 @@ $("<img id='emoji'></img>").attr("src", chrome.extension.getURL('emojione/1f600.
           }
 
           if (smallest_violation > 0) {
+            if (best_index != 3) {
+              jump_allowed = true;
+            }
+            // if(~~(best_index / 2) != last_best_index) {
+            //   last_best_index = ~~(best_index / 2);
+            //   // randomize icon when colliding with a wall facing a different direction
+            //   randomizeEmoji();
+            // }
             // Change the appropriate coordinate in the appropriate direction.
             var original = parseInt($('.chrome-pet-positioner').css(targets[best_index]), 10);
             // console.log("best_index:" + best_index + " smallest_violation:" + signs[best_index] * smallest_violation);
             var modified = original + signs[best_index] * smallest_violation;
             // console.log(targets[best_index] + " modified by " + signs[best_index] * smallest_violation);
             // console.log(modified + 'px');
-            if (best_index == 0)  {
-              console.log("a: ", a + " b:" + b);
-              console.log($(this).parents().map(function(index, elem){
-                if (elem === undefined) {
-                  return undefined;
-                }
-                return $(elem).attr("class") + $(elem).css("position");
-              }));
-            }
+            // if (best_index == 0)  {
+            //   console.log("a: ", a + " b:" + b);
+            //   console.log($(this).parents().map(function(index, elem){
+            //     if (elem === undefined) {
+            //       return undefined;
+            //     }
+            //     return $(elem).attr("class") + $(elem).css("position");
+            //   }));
+            // }
+            // $(".highlight").removeClass("highlight");
+            // $(this).addClass("highlight");
             $('#collision_illustrator').css("left", new_position.left).css("top", new_position.top).css("width", new_size.width).css("height", new_size.height);
+            $('#collision_illustrator_2').css("left", new_position.left).css("top", new_position.top).css("width", new_size.width).css("height", new_size.height);
+            if (a == 0) {
+              var proposed_left = parseInt($('.chrome-pet-positioner').css("left"), 10);
+              var proposed_width = $('.chrome-pet-positioner').width();
+              var actual_left = Math.max(parseInt($('.chrome-pet-positioner').css("left"), 10), new_position.left);
+              var actual_right = Math.min(proposed_left + proposed_width, new_position.left + new_size.width);
+              $('#collision_illustrator_2').css("left", actual_left)
+              .css("width", actual_right - actual_left);
+            } else {
+              var proposed_top = parseInt($('.chrome-pet-positioner').css("top"), 10);
+              var proposed_height = $('.chrome-pet-positioner').height();
+              var actual_top = Math.max(parseInt($('.chrome-pet-positioner').css("top"), 10), new_position.top);
+              var actual_bottom = Math.min(proposed_top + proposed_height, new_position.top + new_size.height);
+              $('#collision_illustrator_2').css("top", actual_top)
+              .css("height", actual_bottom - actual_top);
+            }
             // console.log(violations);
             $('.chrome-pet-positioner').css(targets[best_index], modified + 'px');
             var new_x = parseInt($('.chrome-pet-positioner').offset().left, 10);
@@ -218,8 +327,8 @@ $("<img id='emoji'></img>").attr("src", chrome.extension.getURL('emojione/1f600.
             // $('#position_illustrator').css("height", new_y + 'px');
             // console.log(original_y + ' ' + y);
             // velocity update
-            vx = new_x - x;
-            vy = new_y - y;
+            vx = 0; //new_x - x;
+            vy = 0;//new_y - y;
           }
         }
       }
@@ -234,7 +343,7 @@ $("<img id='emoji'></img>").attr("src", chrome.extension.getURL('emojione/1f600.
 
     new_x = parseInt($temp.offset().left, 10);
     new_y = parseInt($temp.offset().top, 10);
-    console.log($temp.offset().left + " " + new_x);
+    // console.log($temp.offset().left + " " + new_x);
     $('#bad_position_illustrator').css("width", new_x + 'px');
     $('#bad_position_illustrator').css("height", new_y + 'px');
 
